7c49660f1af9cd997400882676bbcd12ef44bcd7
mai cook and Patrick Kille <mai@>
2020-01-21 15:33:33 -0800

    expand javascript functionality in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
  $("#list").submit(function(event) {  event.preventDefault();
-    var foods = ["food1","food2","food3"];
-    var sortUpper = foods.map(function(food) {
-      return food.toUpperCase(); 
-    });
+  var sortUpper = []
+  var foods = [$("#food1").val(), $("#food2").val(), $("#food3").val()];
+    console.log(foods);
+  foods.forEach(function(food){
+    sortUpper.push(food.toUpperCase());
+    console.log(sortUpper);
     sortUpper.sort();
+    });
+    $("#list").hide();
+    $(".orderedlist").append("<li>" + sortUpper[0] + "</li>" + "<li>" + sortUpper[1] + "</li>" + "<li>" + sortUpper[2] + "</li>");
+  
+  });    
 });
